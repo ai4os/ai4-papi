002964024985147b841d13869f8dca89af8a4442
Ignacio Heredia <iheredia@ifca.unican.es>
2023-05-02 13:21:36 +0200

    fix typo

--- docker/docker_refresh.sh
+++ docker/docker_refresh.sh
@@ -9,7 +9,7 @@ echo "$(tput setaf 3)** Build new image **$(tput sgr0)"
 docker build -t ai4-papi /home/ubuntu/ai4-papi/docker
 
 echo "$(tput setaf 3)** Stop old container **$(tput sgr0)"
-docker stop $(sudo docker ps | awk '/ai4-papi/ { print $1 }') || true
+docker stop $(sudo docker ps | awk '/ai4papi/ { print $1 }') || true
 
 echo "$(tput setaf 3)** Start new container **$(tput sgr0)"
 docker run -d -v /home/ubuntu/nomad-certs:/home/nomad-certs -v /home/ubuntu/letsencrypt:/home/letsencrypt -p 443:443 ai4-papi
