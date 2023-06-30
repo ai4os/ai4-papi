2c10507d3201c37d2f1350fb2df55f4d086e149a
Ignacio Heredia <iheredia@ifca.unican.es>
2023-06-30 16:44:06 +0200

    add federated server module

--- ai4papi/conf.py
+++ ai4papi/conf.py
@@ -8,6 +8,8 @@ import nomad
 import yaml
 
 
+Nomad = nomad.Nomad()
+
 # Paths
 main_path = Path(__file__).parent.absolute()
 paths = {
@@ -16,24 +18,49 @@ paths = {
     }
 
 # Load main API configuration
-with open(paths['conf'] / 'main_conf.yaml', 'r') as f:
+with open(paths['conf'] / 'main.yaml', 'r') as f:
     MAIN_CONF = yaml.safe_load(f)
 
+####################
+# Standard modules #
+####################
+
 # Load default Nomad job configuration
-Nomad = nomad.Nomad()
-with open(paths['conf'] / 'job.nomad', 'r') as f:
-    job_raw_nomad = f.read()
-    NOMAD_JOB_CONF = Nomad.jobs.parse(job_raw_nomad)
+with open(paths['conf'] / 'module' / 'module.nomad', 'r') as f:
+    raw_job = f.read()
+    NOMAD_MODULE_CONF = Nomad.jobs.parse(raw_job)
 
 # Load user customizable parameters
-with open(paths['conf'] / 'userconf.yaml', 'r') as f:
-    USER_CONF = yaml.safe_load(f)
+with open(paths['conf'] / 'module' / 'module.yaml', 'r') as f:
+    USER_MODULE_CONF = yaml.safe_load(f)
 
-USER_CONF_VALUES = {}
-for group_name, params in USER_CONF.items():
-    USER_CONF_VALUES[group_name] = {}
+USER_MODULE_VALUES = {}
+for group_name, params in USER_MODULE_CONF.items():
+    USER_MODULE_VALUES[group_name] = {}
     for k, v in params.items():
         assert 'name' in v.keys(), f"Parameter {k} needs to have a name."
         assert 'value' in v.keys(), f"Parameter {k} needs to have a value."
 
-        USER_CONF_VALUES[group_name][k] = v['value']
+        USER_MODULE_VALUES[group_name][k] = v['value']
+
+####################
+# Federated server #
+####################
+
+# Load default Nomad job configuration
+with open(paths['conf'] / 'federated' / 'federated.nomad', 'r') as f:
+    raw_job = f.read()
+    NOMAD_FED_CONF = Nomad.jobs.parse(raw_job)
+
+# Load user customizable parameters
+with open(paths['conf'] / 'federated' / 'federated.yaml', 'r') as f:
+    USER_FED_CONF = yaml.safe_load(f)
+
+USER_FED_VALUES = {}
+for group_name, params in USER_FED_CONF.items():
+    USER_FED_VALUES[group_name] = {}
+    for k, v in params.items():
+        assert 'name' in v.keys(), f"Parameter {k} needs to have a name."
+        assert 'value' in v.keys(), f"Parameter {k} needs to have a value."
+
+        USER_FED_VALUES[group_name][k] = v['value']
