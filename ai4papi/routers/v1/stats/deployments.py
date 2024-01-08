21a1db93c2073c1cb339db5999e98e8449b22612
Ignacio Heredia <iheredia@ifca.unican.es>
2024-01-08 15:23:01 +0100

    feat: add cluster stats
    * Add endpoint to get cluster stats
    
    * Add function to get gpu flavours
    
    * Get the resources without using flavours csv
    
    * refactor: clean implementation
    
    * fix: cache response
    
    * fix: properly count gpus
    
    ---------
    
    Co-authored-by: Marta Obregón <obregonm@ifca.unican.es>

--- ai4papi/routers/v1/stats/deployments.py
+++ ai4papi/routers/v1/stats/deployments.py
@@ -1,14 +1,21 @@
+"""
+Return stats from the user/VO/cluster
+"""
+
 import copy
 import csv
 import os
 from pathlib import Path
+import types
 
 from cachetools import cached, TTLCache
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import HTTPBearer
+import nomad
 
 from ai4papi import auth
 import ai4papi.conf as papiconf
+import ai4papi.nomad.patches as npatches
 
 
 router = APIRouter(
@@ -17,6 +24,14 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 security = HTTPBearer()
+
+main_dir = Path(__file__).resolve().parent
+
+Nomad = nomad.Nomad()
+Nomad.job.get_allocations = types.MethodType(
+    npatches.get_allocations,
+    Nomad.job
+)
 
 
 @cached(cache=TTLCache(maxsize=1024, ttl=6*60*60))
@@ -57,8 +72,8 @@ def load_stats(
     return stats
 
 
-@router.get("/")
-def get_stats(
+@router.get("/user")
+def get_user_stats(
     vo: str,
     authorization=Depends(security),
     ):
@@ -91,3 +106,128 @@ def get_stats(
         user_stats['users-agg'] = None
 
     return user_stats
+
+
+def get_proper_allocation(allocs):
+
+        # Reorder allocations based on recency
+        dates = [a['CreateTime'] for a in allocs]
+        allocs = [x for _, x in sorted(
+            zip(dates, allocs),
+            key=lambda pair: pair[0],
+            )][::-1]  # more recent first
+
+        # Select the proper allocation
+        statuses = [a['ClientStatus'] for a in allocs]
+        if 'unknown' in statuses:
+            # The node has lost connection. Avoid showing temporary reallocated job,
+            # to avoid confusions when the original allocation is restored back again.
+            idx = statuses.index('unknown')
+        elif 'running' in statuses:
+            # If an allocation is running, return that allocation
+            # It happens that after a network cut, when the network is restored,
+            # the temporary allocation created in the meantime (now with status
+            # 'complete') is more recent than the original allocation that we
+            # recovered (with status 'running'), so using only recency does not work.
+            idx = statuses.index('running')
+        else:
+            # Return most recent allocation
+            idx = 0
+
+        return allocs[idx]['ID']
+
+
+@cached(cache=TTLCache(maxsize=1024, ttl=6*60*60))
+@router.get("/cluster")
+def get_cluster_stats():
+    """
+    Returns the following stats of the nodes and the cluster (per resource type):
+    * the aggregated usage
+    * the total capacity
+    """
+
+    resources = [
+        'cpu-total',
+        'cpu-used',
+        'gpu-total',
+        'gpu-used',
+        'ram-total',
+        'ram-used',
+        'disk-total',
+        'disk-used',
+    ]
+    stats = {
+        'nodes' : {},  # individual node usage
+        'cluster': {k: 0 for k in resources},  # aggregated cluster usage
+        }
+
+    # Load nodes
+    nodes = Nomad.nodes.get_nodes(resources=True)
+
+    # Get total stats for each node
+    for n in nodes:
+        node = Nomad.node.get_node(n['ID'])
+
+        n_stats = {k: 0 for k in resources}
+
+        n_stats['cpu-total'] = int(node['Attributes']['cpu.numcores'])
+        n_stats['ram-total'] = int(node['Attributes']['memory.totalbytes'])
+        n_stats['disk-total'] = int(node['Attributes']['unique.storage.bytestotal'])
+        n_stats['disk-used'] = int(node['Attributes']['unique.storage.bytesfree'])
+        if n['NodeResources']['Devices']:
+            for devices in n['NodeResources']['Devices']:
+                if devices['Type'] == 'gpu':
+                    n_stats['gpu-total'] += len(devices['Instances'])
+
+        stats['nodes'][n['ID']] = n_stats
+
+    # Get aggregated usage stats for each node
+    namespaces = ['default', 'ai4eosc', 'imagine', 'tutorials']
+
+    for namespace in namespaces:
+        jobs = Nomad.jobs.get_jobs(namespace=namespace, filter_='Status == "running"')
+        for j in jobs:
+
+            # Retrieve full job for meta
+            job = Nomad.job.get_job(
+                id_=j['ID'],
+                namespace=namespace,
+                )
+
+            allocs = Nomad.job.get_allocations(
+                id_=job['ID'],
+                namespace=namespace,
+                )
+
+            # Keep the proper allocation
+            a = Nomad.allocation.get_allocation(
+                get_proper_allocation(allocs)
+                )
+
+            # Add resources
+            n_stats = stats['nodes'][a['NodeID']]
+            #FIXME: we are ignoring resources consumed by other tasks
+            if 'usertask' in a['AllocatedResources']['Tasks']:
+                res = a['AllocatedResources']['Tasks']['usertask']
+
+                # cpu
+                if res['Cpu']['ReservedCores']:
+                    n_stats['cpu-used'] += len(res['Cpu']['ReservedCores'])
+
+                # ram
+                n_stats['ram-used'] += res['Memory']['MemoryMB']
+
+                # gpu
+                if res['Devices']:
+                    gpu = [d for d in res['Devices'] if d['Type'] == 'gpu'][0]
+                    gpu_num = len(gpu['DeviceIDs']) if gpu else 0
+                    n_stats['gpu-used'] += gpu_num
+            else:
+                continue
+
+    # Compute cluster stats
+    for n_stats in stats['nodes'].values():
+        for k, v in n_stats.items():
+            stats['cluster'][k] += v
+
+    return stats
