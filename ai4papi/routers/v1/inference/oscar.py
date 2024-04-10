3e0b5b8b8594bae53e0e56d5c259dbd405f3d891
Ignacio Heredia <iheredia@ifca.unican.es>
2024-04-10 13:57:30 +0200

    style: improve style

--- ai4papi/routers/v1/inference/oscar.py
+++ ai4papi/routers/v1/inference/oscar.py
@@ -1,14 +1,19 @@
 """
 Manage OSCAR clusters to create and execute services.
-from typing import List
-from fastapi import APIRouter, Depends, FastAPI, HTTPException
-from fastapi.security import HTTPBearer
-from typing import Tuple, Union
-from ai4papi import auth, utils
+"""
 import json
 import re
-from ai4papi.conf import MAIN_CONF
+from typing import List
+
+
+from fastapi import APIRouter, Depends, FastAPI, HTTPException
+from fastapi.security import HTTPBearer
+from oscar_python.client import Client
 from pydantic import BaseModel
+
+from ai4papi import auth, utils
+from ai4papi.conf import MAIN_CONF
+
 
 router = APIRouter(
     prefix="/oscar",
@@ -27,7 +32,7 @@ class Service(BaseModel):
     model_config = {
         "json_schema_extra": {
             "examples": [
-                {            
+                {
                     "name": "string",
                     "memory": "string",
                     "cpu": "string",
@@ -42,30 +47,36 @@ class Service(BaseModel):
 
 security = HTTPBearer()
 
+
 def get_client_from_auth(authorization, cluster_id, vo):
-    # Retrieve authenticated user info
+    """
+    Retrieve authenticated user info
+    """
     oidc_token = authorization.credentials
     auth_info = auth.get_user_info(token=oidc_token)
     auth.check_vo_membership(vo, auth_info['vos'])
 
     try:
         endpoint = MAIN_CONF["oscar"]["clusters"][cluster_id]["endpoint"]
-    except:
+    except Exception:
         raise HTTPException(
-                status_code=400,
-                detail=f"The provided cluster id '{cluster_id}' is not listed on the configuration file"
+            status_code=400,
+            detail=f"The provided cluster id '{cluster_id}' is not listed on the" \
+                    "configuration file.",
         )
 
     client_options = {
-                    'cluster_id': cluster_id,
-                    'endpoint': endpoint,
-                    'oidc_token': oidc_token,
-                    'ssl': 'true'}
+        'cluster_id': cluster_id,
+        'endpoint': endpoint,
+        'oidc_token': oidc_token,
+        'ssl': 'true',
+        }
 
     try:
         return Client(client_options)
-    except:
+    except Exception:
         raise Exception("Error creating OSCAR client")
+
 
 def make_service_definition(svc_conf, cluster_id):
 
@@ -74,7 +85,11 @@ def make_service_definition(svc_conf, cluster_id):
     service_definition["memory"] = svc_conf.memory
     service_definition["cpu"] = svc_conf.cpu
     service_definition["image"] = svc_conf.image
-    service_definition["script"] = re.sub('.type','.'+svc_conf.input_type, service_definition["script"])
+    service_definition["script"] = re.sub(
+        '.type',
+        '.'+svc_conf.input_type,
+        service_definition["script"]
+        )
     service_definition["allowed_users"] = svc_conf.allowed_users
 
     endpoint = MAIN_CONF["oscar"]["clusters"][cluster_id]["endpoint"]
@@ -82,25 +97,32 @@ def make_service_definition(svc_conf, cluster_id):
 
     return service_definition, service_url
 
-# Gets information about the cluster.
-#  - Returns a JSON with the cluster information.
-@router.get("/cluster/{cluster_id}")
-def get_cluster_info(cluster_id: str,
-                        vo: str,
-                        authorization=Depends(security)):
 
+@router.get("/cluster/{cluster_id}")
+def get_cluster_info(
+    cluster_id: str,
+    vo: str,
+    authorization=Depends(security),
+    ):
+    """
+    Gets information about the cluster.
+    - Returns a JSON with the cluster information.
+    """
     client = get_client_from_auth(authorization, cluster_id, vo)
     result = client.get_cluster_info()
     return (result.status_code, json.loads(result.text))
 
 
-# Creates a new inference service for an AI pre-trained model on a specific cluster
 @router.post("/services/{cluster_id}")
-def create_service(cluster_id: str,
-                    vo: str,
-                    svc_conf: Service,
-                    authorization=Depends(security)):
-
+def create_service(
+    cluster_id: str,
+    vo: str,
+    svc_conf: Service,
+    authorization=Depends(security),
+    ):
+    """
+    Creates a new inference service for an AI pre-trained model on a specific cluster
+    """
     client = get_client_from_auth(authorization, cluster_id, vo)
 
     # If authentication doesn't fail set user vo on the service
@@ -114,13 +136,16 @@ def create_service(cluster_id: str,
         return (result.status_code, None)
 
 
-# Updates service if it exists
 @router.put("/services/{cluster_id}/{service_name}")
-def update_service(cluster_id: str,
-                    vo: str,
-                    svc_conf: Service,
-                    authorization=Depends(security)):
-
+def update_service(
+    cluster_id: str,
+    vo: str,
+    svc_conf: Service,
+    authorization=Depends(security),
+    ):
+    """
+    Updates service if it exists
+    """
     client = get_client_from_auth(authorization, cluster_id, vo)
 
     # If authentication doesn't fail set user vo on the service
@@ -135,54 +160,67 @@ def update_service(cluster_id: str,
     else:
         return (result.status_code, None)
 
-# Retrieves a list of all the deployed services of the cluster.
-#  - Returns a JSON with the cluster information.
-@router.get("/services/{cluster_id}")
-def get_services_list(cluster_id: str,
-                        vo: str,
-                        authorization=Depends(security)):
 
+@router.get("/services/{cluster_id}")
+def get_services_list(
+    cluster_id: str,
+    vo: str,
+    authorization=Depends(security),
+    ):
+    """
+    Retrieves a list of all the deployed services of the cluster.
+    - Returns a JSON with the cluster information.
+    """
     client = get_client_from_auth(authorization, cluster_id, vo)
     result = client.list_services()
-
     return (result.status_code, json.loads(result.text))
 
-# Retrieves a specific service.
-#  - Returns a JSON with the cluster information.
-@router.get("/services/{cluster_id}/{service_name}")
-def get_service(cluster_id: str,
-                vo: str,
-                service_name: str,
-                authorization=Depends(security)):
 
+@router.get("/services/{cluster_id}/{service_name}")
+def get_service(
+    cluster_id: str,
+    vo: str,
+    service_name: str,
+    authorization=Depends(security),
+    ):
+    """
+    Retrieves a specific service.
+    - Returns a JSON with the cluster information.
+    """
     client = get_client_from_auth(authorization, cluster_id, vo)
     result = client.get_service(service_name)
-
     return (result.status_code, json.loads(result.text))
 
-# Delete a specific service.
+
 @router.delete("/services/{cluster_id}/{service_name}")
-def delete_service(cluster_id: str,
-                    vo: str,
-                    service_name: str,
-                    authorization=Depends(security)):
-
+def delete_service(
+    cluster_id: str,
+    vo: str,
+    service_name: str,
+    authorization=Depends(security),
+    ):
+    """
+    Delete a specific service.
+    """
     client = get_client_from_auth(authorization, cluster_id, vo)
-
     result = client.remove_service(service_name)
     return (result.status_code,service_name)
 
-#Make a synchronous execution (inference)
-@router.post("/services/{cluster_id}/{service_name}")
-def inference(cluster_id: str,
-                vo: str,
-                service_name: str,
-                data: dict,
-                authorization=Depends(security)):
-    client = get_client_from_auth(authorization, cluster_id, vo)
 
+@router.post("/services/{cluster_id}/{service_name}")
+def inference(
+    cluster_id: str,
+    vo: str,
+    service_name: str,
+    data: dict,
+    authorization=Depends(security),
+    ):
+    """
+    Make a synchronous execution (inference)
+    """
+    client = get_client_from_auth(authorization, cluster_id, vo)
     result = client.run_service(service_name, input=data["input_data"])
     try:
         return (result.status_code, json.loads(result.text))
-    except:
+    except Exception:
         return (result.status_code, result.text)
