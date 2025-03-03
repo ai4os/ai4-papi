fd9a172b2570bb612aaf9d3b3f572758583a4aaf
stevo <stefan.dlugolinsky@savba.sk>
2025-03-03 09:27:57 +0100

    code readability change

--- ai4papi/routers/v1/deployments/tools.py
+++ ai4papi/routers/v1/deployments/tools.py
@@ -88,6 +88,20 @@ def get_deployments(
 
     return sorted_jobs
 
+def remove_job_endpoints(
+    job,
+    endpoints=[]
+):
+    """ Remove useless endpoints (they all point to same url)
+    """
+    job["endpoints"] = {
+        k: v for k, v in job["endpoints"].items() if k not in endpoints
+    }
+    if job["active_endpoints"]:
+        job["active_endpoints"] = [
+            k for k in job["active_endpoints"] if k not in endpoints
+        ]
+    return job
 
 @router.get("/{deployment_uuid}")
 def get_deployment(
@@ -138,15 +152,7 @@ def get_deployment(
 
     # Additional checks
     if tool_id == "ai4os-cvat":
-        # Remove useless endpoints (they all point to same url)
-        ignore = ["server", "grafana"]
-        job["endpoints"] = {
-            k: v for k, v in job["endpoints"].items() if k not in ignore
-        }
-        if job["active_endpoints"]:
-            job["active_endpoints"] = [
-                k for k in job["active_endpoints"] if k not in ignore
-            ]
+        job = remove_job_endpoints(job, endpoints = ["server", "grafana"])
 
     if tool_id == "ai4os-ai4life-loader":
         job["main_endpoint"] = "ui"  # instead of deepaas
