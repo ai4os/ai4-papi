bfe9ae134ba8dec8276d58b493cb76a4eae80ee8
Marta Obregón <obregonm@ifca.unican.es>
2025-12-03 13:35:27 +0100

    style: pre-commit

--- ai4papi/routers/v1/proxies/apisix.py
+++ ai4papi/routers/v1/proxies/apisix.py
@@ -6,12 +6,11 @@ with APISIX Admin API. The request to our API is authenticated with
 a Bearer token. The internal call to APISIX uses the APISIX admin API token.
 """
 
-
 import os
 import requests
 import secrets
 import json
-from fastapi import APIRouter, HTTPException, Depends, Request
+from fastapi import APIRouter, HTTPException, Depends
 from fastapi.security import HTTPBearer
 
 from ai4papi import auth
@@ -40,13 +39,12 @@ if not APISIX_API_KEY:
         print('"APISIX_API_KEY" envar is not defined')
     else:
         raise Exception('You need to define the variable "APISIX_API_KEY".')
-        
+
 # Session used only to call APISIX
 session = requests.Session()
-session.headers.update({
-    "X-API-KEY": APISIX_API_KEY,
-    "Content-Type": "application/json"
-})
+session.headers.update(
+    {"X-API-KEY": APISIX_API_KEY, "Content-Type": "application/json"}
+)
 
 
 def list_consumers():
@@ -56,7 +54,7 @@ def list_consumers():
     resp = session.get(f"{APISIX_URL}/consumers")
     if not resp.ok:
         raise HTTPException(status_code=resp.status_code, detail=json.loads(resp.text))
-    
+
     data = resp.json()
     usernames = [item["value"]["username"] for item in data.get("list", [])]
     return usernames
@@ -67,49 +65,39 @@ def add_credential(user_id: str, name: str):
     Add new credential to consumer in APISIX.
     """
     token = secrets.token_urlsafe(32)
-    credential_data = {
-        "plugins": {
-            "key-auth": {
-                "key": token
-            }
-        }
-    }
-    
+    credential_data = {"plugins": {"key-auth": {"key": token}}}
+
     resp = session.put(
-        f"{APISIX_URL}/consumers/{user_id}/credentials/{name}",
-        json=credential_data
+        f"{APISIX_URL}/consumers/{user_id}/credentials/{name}", json=credential_data
     )
-    
+
     if not resp.ok:
         raise HTTPException(status_code=resp.status_code, detail=json.loads(resp.text))
-    
+
     return token
 
 
 @router.get("/api_keys")
-def get_api_keys(
-    authorization=Depends(security)
-):
+def get_api_keys(authorization=Depends(security)):
     """
     Retrieve existing credentials for a consumer in APISIX.
     """
     # Retrieve authenticated user info
     auth_info = auth.get_user_info(token=authorization.credentials)
-        
-    resp = session.get(
-        f"{APISIX_URL}/consumers/{auth_info["id"]}/credentials")
-    
+
+    resp = session.get(f"{APISIX_URL}/consumers/{auth_info['id']}/credentials")
+
     if resp.status_code == 404:
         result = []
     elif not resp.ok:
         raise HTTPException(status_code=resp.status_code, detail=json.loads(resp.text))
-    else: 
+    else:
         credentials = json.loads(resp.text)
         result = [
-        {
-            "id": item["value"]["id"],
-            "api_key": item["value"]["plugins"]["key-auth"]["key"]
-        }
+            {
+                "id": item["value"]["id"],
+                "api_key": item["value"]["plugins"]["key-auth"]["key"],
+            }
             for item in credentials["list"]
         ]
 
@@ -117,60 +105,51 @@ def get_api_keys(
 
 
 @router.post("/api_keys")
-def create_api_key(
-    name: str,
-    authorization=Depends(security)
-):
+def create_api_key(name: str, authorization=Depends(security)):
     """
     Create a new credential in APISIX.
 
-    If consumer exists, adds the new credential using credentials endpoint. 
+    If consumer exists, adds the new credential using credentials endpoint.
     If consumer does not exist, creates the consumer using the consumers endpoint
     and then their first credential using the credentials endpoint [1].
-    
+
     **Notes**:
     [1]: https://apisix.apache.org/docs/apisix/terminology/credential/#example
     """
     # Retrieve authenticated user info
     auth_info = auth.get_user_info(token=authorization.credentials)
-    
+
     consumers = list_consumers()
-    
-    if auth_info['id'] not in consumers:
+
+    if auth_info["id"] not in consumers:
         print(f"Consumer '{auth_info['id']}' does not exist. Creating consumer.")
-        consumer_data = {
-            "username": auth_info['id']
-        }
-        
-        resp = session.put(
-            f"{APISIX_URL}/consumers",
-            json=consumer_data
-        )
-        
+        consumer_data = {"username": auth_info["id"]}
+
+        resp = session.put(f"{APISIX_URL}/consumers", json=consumer_data)
+
         if not resp.ok:
-            raise HTTPException(status_code=resp.status_code, detail=json.loads(resp.text))
-        
+            raise HTTPException(
+                status_code=resp.status_code, detail=json.loads(resp.text)
+            )
+
     token = add_credential(auth_info["id"], name)
-    
+
     return {token}
 
 
 @router.delete("/api_keys")
-def delete_api_key(
-    name: str,
-    authorization=Depends(security)
-):
+def delete_api_key(name: str, authorization=Depends(security)):
     """
     Delete a credential in APISIX.
     """
     # Retrieve authenticated user info
     auth_info = auth.get_user_info(token=authorization.credentials)
-  
+
     resp = session.delete(
-        f"{APISIX_URL}/consumers/{auth_info['id']}/credentials/{name}")
-        
+        f"{APISIX_URL}/consumers/{auth_info['id']}/credentials/{name}"
+    )
+
     if not resp.ok:
         raise HTTPException(status_code=resp.status_code, detail=json.loads(resp.text))
-        
-    
+
     return {"status": "success"}
