305c5710da819f991d8f5c606f8b55ce7424db56
Ignacio Heredia <iheredia@ifca.unican.es>
2025-12-11 15:40:38 +0100

    test: allow resume testing

--- tests/main.py
+++ tests/main.py
@@ -10,8 +10,7 @@ Nomad (ie. after launching)
 # TODO: move to proper testing package
 # TODO: add spinners
 
-import glob
-import os
+from pathlib import Path
 
 import ai4papi.conf as papiconf
 
@@ -19,10 +18,15 @@ import ai4papi.conf as papiconf
 # We want to test full functionality, without disabling any parts
 papiconf.IS_DEV = False
 
-
 # Import all test files dynamically
-test_files = glob.glob(os.path.join(os.path.dirname(__file__), "test_*.py"))
-test_files.sort()
-for test_file in test_files:
-    module_name = os.path.basename(test_file)[:-3]  # Remove .py extension
-    __import__(module_name)
+test_files = sorted(Path(__file__).parent.glob("test_*.py"))
+modules = [f.stem for f in test_files]
+
+# Ignore tests that have already passed in a previous run
+resume_from = None
+# resume_from = "test_routes"
+if resume_from:
+    modules = modules[modules.index(resume_from) :]
+
+for module in modules:
+    __import__(module)
